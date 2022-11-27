88eb270b731c3738ef63ba607fa080ecc122f2f6
Gustafa Bayu <faafa13@gmail.com>
2022-11-27 17:56:18 +0700

    stage 8 - scroll smooth

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,11 +1,16 @@
 window.onscroll = function () {
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
+    const topPage = document.querySelector('#top-page');
 
     if (window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed');
+        topPage.classList.remove('hidden');
+        topPage.classList.add('flex');
     } else {
         header.classList.remove('navbar-fixed');
+        topPage.classList.remove('flex');
+        topPage.classList.add('hidden');
     }
 }
 
@@ -16,3 +21,10 @@ hamburger.addEventListener('click', function () {
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 });
+
+window.addEventListener('click', function (e) {
+    if (e.target != hamburger && e.target != navMenu) {
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
+    }
+})
